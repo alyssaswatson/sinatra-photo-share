1130881ea8bc2575be05019fbda8bd4c7e79616e
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-04-01 03:10:56 +0000

    Initial files

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,16 @@
+require './config/environment'
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
+  end
+
+  get '/' do
+    "hello World"
+  end
+
+end
